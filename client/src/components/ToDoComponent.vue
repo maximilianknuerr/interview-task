e7f703cdc990edbc02db3a145bec0153c07935ff
Maximilian <maximilian.knuerr@googlemail.com>
2019-10-31 13:45:00 +0100

    frontend-backend connection

---
+++ client/src/components/ToDoComponent.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container" >
+    <h1>ToDo List</h1>
+    <div class="create-post">
+      <input v-on:keyup.enter="createPost" type="text" id="create-post" v-model="text" placeholder="Create a ToDo">
+    </div>
+    <hr>
+    <p class="error" v-if="error">{{ error }}</p>
+    <div class="post-container">
+      <div class = "post"
+        v-for="(post, index) in posts"
+        v-bind:item="post"
+        v-bind:index="index"
+        v-bind:key="post._id"
+        
+      >
+        {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}` }}
+        <p class="text">{{ post.text }}</p> 
+        <button class="delete" v-on:click="deletePost(post._id)">x</button>
+      </div> 
+    </div>
+  </div>
+
+</template>
+
+<script>
+import PostService from '../PostService'
+export default {
+  name: 'ToDoComponent',
+  data() {
+    return {
+      posts: [],
+      error: '',
+      text: ''
+    }
+  },
+  async created() {
+    try {
+      this.posts = await PostService.getPosts()
+    } catch(err) {
+      this.error = err.message
+    }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text)
+      this.text = ""
+      this.posts = await PostService.getPosts()
+    },
+    async deletePost(id) {
+      await PostService.deletePost(id)
+      this.posts = await PostService.getPosts()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button.delete {
+  width: 30px;
+  height: 30px;
+  font-size: 29px;
+  text-align: center;
+  line-height: 1px;
+}
+input {
+  width: 95%;
+  height: 30px;
+}
+div.container {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+p.error {
+  border: 1px solid #ff5b5f;
+  background-color: #ffc5c1;
+  padding:  10px;
+  margin-bottom: 15px;
+}
+
+div.post {
+  position: relative;
+  border: 1px solid grey;
+  background-color:  white;
+  padding: 10px 10px 30px 10px;
+  margin-bottom: 15px;
+}
+
+div.created-at {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 5px 15px 5px 15px;
+  background-color: darkgreen;
+  color: white;
+  font-size: 13px;
+}
+
+p.text {
+  font-size: 22px;
+  font-weight: 700;
+  margin-bottom: 0;
+}
+</style>
